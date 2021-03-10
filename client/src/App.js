d75a6210fec3aa75e442b4e323c5c29d180b8aa1
lucabettini <bettiniluca1@gmail.com>
2021-03-10 16:51:21 +0100

    Switched to hashrouter, materialize still not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
