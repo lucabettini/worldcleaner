8259be1dea0bb2481e7fe616d07fcb2b42f102dc
lucabettini <bettiniluca1@gmail.com>
2021-03-12 20:19:47 +0100

    Specified root

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use('/api/clean', cleanRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile('client/build/index.html');
+    res.sendFile('client/build/index.html', { root: __dirname });
   });
 }
 
